7b60ae8dccd1b3578e3c1ba061a32720b6bf2cc2
jgarcia-r7 <81575551+jgarcia-r7@users.noreply.github.com>
2022-04-15 18:21:01 -0400

    Create dork.py

---
+++ modules/auxiliary/dork.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Module: Google Dorking Module
+# Author: Jessi
+from colorama import Fore, Style
+from googlesearch import search
+from time import sleep
+
+# Define colorama colors.
+GREEN = Fore.GREEN
+RED = Fore.RED
+WHITE = Fore.WHITE
+YELLOW = Fore.YELLOW
+CYAN = Fore.CYAN
+PINK = Fore.MAGENTA
+BRIGHT = Style.BRIGHT
+DIM = Style.DIM
+NORM = Style.NORMAL
+RST = Style.RESET_ALL
+
+# Dork class.
+class pyDork:
+
+    # Module specific settings.
+    query = ''
+    limit = 10
+    outfile = 'google_dork.txt'
+
+    @classmethod
+    def print_help(cls):
+        module_help = f"""
+
+Module Options (auxiliary/google_dork):
+                
+    Name                  Description                 Current Setting
+    ----                  -----------                 ---------------
+    QUERY                 Desired query               {cls.query}
+    LIMIT                 Results limit               {cls.limit}
+    OUTFILE               Output file                 {cls.outfile}
+
+                """
+        print(module_help)
+
+    @classmethod
+    def init_query(cls):
+        f = open(cls.outfile, "wt")
+        for result in search(cls.query, num_results=cls.limit):
+            print(GREEN + "[+]" + RST + " {}".format(result))
+            f.write(result + "\n")
+            sleep(.30)
+        print(GREEN + "[+]" + RST + " Results written to {}\n".format(cls.outfile))
+
+
+def main():
+    print(CYAN + "\n[*]" + RST + " Running query: " + pyDork.query + " with limit " + str(pyDork.limit) + "...")
+    pyDork.init_query()
