e98203ae3dc448525c17f2fcaeeece31bcd71fdc
jgarcia-r7 <81575551+jgarcia-r7@users.noreply.github.com>
2022-04-15 18:21:48 -0400

    Create intelx.py

---
+++ modules/auxiliary/intelx.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Module: Intelx
+from ast import In
+import requests
+from colorama import Fore, Style
+
+
+# Define colorama colors.
+GREEN = Fore.GREEN
+RED = Fore.RED
+WHITE = Fore.WHITE
+YELLOW = Fore.YELLOW
+CYAN = Fore.CYAN
+PINK = Fore.MAGENTA
+BRIGHT = Style.BRIGHT
+DIM = Style.DIM
+NORM = Style.NORMAL
+RST = Style.RESET_ALL
+
+# intelx class
+class IntelX:
+
+    r_api_key = requests.get("https://phonebook.cz",headers={"User-Agent":"Mozilla/5.0"}) # Requsts latest api key from phonebook.cz.
+    for line in r_api_key.text.split("\n"):
+        if "var API_KEY" in line:
+            api_key = line.replace("var API_KEY = '","").replace("';","").replace(" ","").strip() # Strip api key from request text.
+    api_url = 'public.intelx.io'
+    headers = {"User-Agent": "Mozilla/5.0", "Accept-Encoding": "gzip, deflate", "Accept": "*/*", "Origin": "https://phonebook.cz", "Referer": "https://phonebook.cz/", "Content-Type": "application/x-www-form-urlencoded"}
+
+    domain = ''
+    limit = 10
+    outfile = '_emails.txt'
+
+    @classmethod
+    def print_help(cls):
+       module_help =f"""
+
+Module Options (auxiliary/intelx_search):
+                
+    Name                  Description                 Current Setting
+    ----                  -----------                 ---------------
+    DOMAIN                Target domain               {cls.domain}
+    LIMIT                 Results limit               {cls.limit}
+    OUTFILE               Output file                 {cls.outfile}
+
+       """
+       print(module_help)
+   
+    @classmethod
+    def search(cls):
+
+        key_payload = {"k":cls.api_key}
+        data_payload = {"maxresults": cls.limit, "media": 0, "target": 2, "term": cls.domain, "terminate": [None], "timeout": 20}
+        r_id = requests.post(f'https://{cls.api_url}/phonebook/search', headers=cls.headers, params=key_payload, json=data_payload)
+        id = r_id.json()["id"]
+        print(GREEN + "[+]" + RST + " Search ID Retrieved: {}".format(id))
+
+        results_payload = {"k":cls.api_key, "id":id, "limit": cls.limit}
+        r_results = requests.get(f'https://{cls.api_url}/phonebook/search/result', headers=cls.headers, params=results_payload)
+        storage = r_results.json()
+
+        w = open(cls.outfile, 'w+')
+        for each in storage["selectors"]:
+            w.write(each["selectorvalue"])
+            w.write("\n")
+        w.close()
+
+        emailSum = 0
+        with open(cls.outfile) as emails:
+            for email in emails:
+                if email.strip():
+                    emailSum += 1
+        print(GREEN + "[+]" + RST + " Found {} email addresses!".format(emailSum))
+        print(GREEN + "[+]" + RST + " Results written to {}\n".format(cls.outfile))
+
+
+def main():
+    print(CYAN + "\n[*]" + RST + " Searching for email addressess for domain: " + IntelX.domain + " with limit " + str(IntelX.limit) + "...")
+    IntelX.search()
